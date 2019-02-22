f2e88374036b6b79b0312c7b82f142effa87c13d
Maxime ROBERT <maxime.robert@smile.fr>
2019-02-22 13:59:51 +0100

    Update pokemon images (+ Deoxys)

--- scripts/dlPokemonImg.js
+++ scripts/dlPokemonImg.js
@@ -8,114 +8,141 @@ const FORMS = {
   // // Zarbi
   // 201: {urlMin: 11, fsMin: 1, nb: 26},
   // Morphéo
-  351: {urlMin: 11, fsMin: 29, nb: 4}
+  351: { urlMin: 11, fsMin: 29, nb: 4 },
+  // Deoxys
+  386: { urlMin: 11, fsIds: [386, 10001, 10002, 10003], nb: 4 }
 };
 
-const ALOLA = [{
-  "pid": 10091,
-  "name": "Rattata Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Rattata",
-  "originalId": 19
-}, {
-  "pid": 10092,
-  "name": "Rattatac Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Rattatac",
-  "originalId": 20
-}, {
-  "pid": 10100,
-  "name": "Raichu Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Raichu",
-  "originalId": 26
-}, {
-  "pid": 10101,
-  "name": "Sabelette Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Sabelette",
-  "originalId": 27
-}, {
-  "pid": 10102,
-  "name": "Sablaireau Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Sablaireau",
-  "originalId": 28
-}, {
-  "pid": 10103,
-  "name": "Goupix Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Goupix",
-  "originalId": 37
-}, {
-  "pid": 10104,
-  "name": "Feunard Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Feunard",
-  "originalId": 38
-}, {
-  "pid": 10105,
-  "name": "Taupiqueur Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Taupiqueur",
-  "originalId": 50
-}, {
-  "pid": 10106,
-  "name": "Triopikeur Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Triopikeur",
-  "originalId": 51
-}, {
-  "pid": 10107,
-  "name": "Miaouss Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Miaouss",
-  "originalId": 52
-}, {
-  "pid": 10108,
-  "name": "Persian Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Persian",
-  "originalId": 53
-}, {
-  "pid": 10109,
-  "name": "Racaillou Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Racaillou",
-  "originalId": 74
-}, {
-  "pid": 10110,
-  "name": "Gravalanch Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Gravalanch",
-  "originalId": 75
-}, {
-  "pid": 10111,
-  "name": "Grolem Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Grolem",
-  "originalId": 76
-}, {
-  "pid": 10112,
-  "name": "Tadmorv Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Tadmorv",
-  "originalId": 88
-}, {
-  "pid": 10113,
-  "name": "Grotadmorv Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Grotadmorv",
-  "originalId": 89
-}, {
-  "pid": 10114,
-  "name": "Noadkoko Forme d’Alola",
-  "locale": "fr",
-  "originalName": "Noadkoko",
-  "originalId": 103
-}, {"pid": 10115, "name": "Ossatueur Forme d’Alola", "locale": "fr", "originalName": "Ossatueur", "originalId": 105}];
+const ALOLA = [
+  {
+    pid: 10091,
+    name: 'Rattata Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Rattata',
+    originalId: 19
+  },
+  {
+    pid: 10092,
+    name: 'Rattatac Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Rattatac',
+    originalId: 20
+  },
+  {
+    pid: 10100,
+    name: 'Raichu Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Raichu',
+    originalId: 26
+  },
+  {
+    pid: 10101,
+    name: 'Sabelette Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Sabelette',
+    originalId: 27
+  },
+  {
+    pid: 10102,
+    name: 'Sablaireau Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Sablaireau',
+    originalId: 28
+  },
+  {
+    pid: 10103,
+    name: 'Goupix Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Goupix',
+    originalId: 37
+  },
+  {
+    pid: 10104,
+    name: 'Feunard Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Feunard',
+    originalId: 38
+  },
+  {
+    pid: 10105,
+    name: 'Taupiqueur Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Taupiqueur',
+    originalId: 50
+  },
+  {
+    pid: 10106,
+    name: 'Triopikeur Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Triopikeur',
+    originalId: 51
+  },
+  {
+    pid: 10107,
+    name: 'Miaouss Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Miaouss',
+    originalId: 52
+  },
+  {
+    pid: 10108,
+    name: 'Persian Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Persian',
+    originalId: 53
+  },
+  {
+    pid: 10109,
+    name: 'Racaillou Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Racaillou',
+    originalId: 74
+  },
+  {
+    pid: 10110,
+    name: 'Gravalanch Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Gravalanch',
+    originalId: 75
+  },
+  {
+    pid: 10111,
+    name: 'Grolem Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Grolem',
+    originalId: 76
+  },
+  {
+    pid: 10112,
+    name: 'Tadmorv Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Tadmorv',
+    originalId: 88
+  },
+  {
+    pid: 10113,
+    name: 'Grotadmorv Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Grotadmorv',
+    originalId: 89
+  },
+  {
+    pid: 10114,
+    name: 'Noadkoko Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Noadkoko',
+    originalId: 103
+  },
+  {
+    pid: 10115,
+    name: 'Ossatueur Forme d’Alola',
+    locale: 'fr',
+    originalName: 'Ossatueur',
+    originalId: 105
+  }
+];
 
-const dl = function (img, fileName) {
+const dl = function(img, fileName) {
   const url = `${BASE_URL}/${img}.png`;
   https.get(url, response => {
     if (response.statusCode === 200) {
@@ -126,7 +153,7 @@ const dl = function (img, fileName) {
   });
 };
 
-const pad = function (value, length) {
+const pad = function(value, length) {
   let pad = '';
   for (let i = 0; i < length; i++) {
     pad += '0';
@@ -134,7 +161,7 @@ const pad = function (value, length) {
   return (pad + value).substring(('' + value).length);
 };
 
-for (let i = 1; i <= 384; i++) {
+for (let i = 1; i <= 488; i++) {
   const id = pad(i, 3);
 
   let img = id;
@@ -146,10 +173,10 @@ for (let i = 1; i <= 384; i++) {
   if (form) {
     for (let fi = 0; fi < form.nb; fi++) {
       const urlForm = pad(form.urlMin + fi, 2);
-      const fsForm = form.fsMin + fi;
+      const formFileName = form.fsMin ? `${fileName}-${form.fsMin + fi}` : form.fsIds[fi];
       toDL.push({
         img: `${img}-${urlForm}`,
-        fileName: `${fileName}-${fsForm}`
+        fileName: formFileName
       });
     }
   } else {
@@ -164,7 +191,7 @@ for (let i = 1; i <= 384; i++) {
     toDL.push({
       img: `${img}-61`,
       fileName: alola.pid
-    })
+    });
   }
 
   toDL.forEach(d => dl(d.img, d.fileName));
